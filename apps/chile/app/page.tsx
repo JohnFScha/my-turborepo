e12f05b489299c7020cd0d4f335305a74d604cee
jfscha <jfschallibaum@gmail.com>
2024-06-25 09:51:28 -0300

    migrado a turbo 2.0 con pnpm

--- apps/chile/app/page.tsx
+++ apps/chile/app/page.tsx
@@ -1,16 +1,4 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import Flexy from "../../../packages/global-assets/flexy.png"
-import VioletShape from "../../../packages/global-assets/violet-shape.png"
-import VioletShapeMobile from "../../../packages/global-assets/violet-shape-mobile.png"
-import BlueSmile from "../../../packages/global-assets/blue-smile.png"
-import VioletSmile from "../../../packages/global-assets/violet-smile.png"
-import GreenSmile from "../../../packages/global-assets/green-smile.png"
-import WhiteSmile from "../../../packages/global-assets/white-smile.png"
-import WhiteHouse from "../../../packages/global-assets/house-white.png"
-import CircleText from "../../../packages/global-assets/circle-text-building-landing.png"
-import ArrowPattern from "../../../packages/global-assets/arrow-pattern.png"
-import PhoneFlexy from "../../../packages/global-assets/phone-flexy-home.png"
 import clsx from "clsx";
  
 export default function Page(): JSX.Element {
@@ -20,21 +8,21 @@ export default function Page(): JSX.Element {
        <div className={styles.textWrapper}>
         <h1 className={styles.title}>¡Próximamente!</h1>
         <span className={styles.subtitle}>Podrás vender o arrendar <br/> tu propiedad.</span>
-        <Image src={Flexy} alt="logo" className={styles.logo} />
+        <img src="/flexy.png" alt="logo" className={styles.logo} />
        </div>
        <div className={styles.animationWrapper}> 
-        <Image src={VioletShape} alt="violet-shape"  className={clsx(styles.shape, styles.absolute, styles.desktop)} />
-        <Image src={VioletShapeMobile} alt="violet-shape"  className={clsx(styles.shape, styles.absolute, styles.mobile)} />
-        <Image src={ArrowPattern} alt="arrow-pattern" className={clsx(styles.arrows, styles.absolute)}  />
-        <Image src={WhiteHouse} alt="white-house" className={clsx(styles.house, styles.absolute)}  />
-        <Image src={VioletSmile} alt="violet-smile"  className={clsx(styles.violetSmile,styles.smiles,styles.smilesLeftTop, styles.absolute)} />
-        <Image src={BlueSmile} alt="blue-smile" className={clsx(styles.blueSmile,styles.smiles, styles.smilesLeftTop, styles.absolute)}  />
-        <Image src={GreenSmile} alt="green-smile" className={clsx(styles.greenSmile,styles.smiles,styles.smilesLeftTop, styles.absolute)}  />
-        <Image src={WhiteSmile} alt="white-smile" className={clsx(styles.whiteSmile,styles.smiles,styles.smilesBottom, styles.absolute)}  />
-        <Image src={GreenSmile} alt="green-smile" className={clsx(styles.greenSmile2,styles.smiles,styles.smilesRight,  styles.absolute)}  />
-        <Image src={WhiteSmile} alt="white-smile" className={clsx(styles.whiteSmile2,styles.smiles, styles.smilesRight, styles.absolute)}  />
-        <Image src={CircleText} alt="circle-text" className={clsx(styles.circleText, styles.absolute)}  />
-        <Image src={PhoneFlexy} alt="phone-flexy"  className={clsx(styles.phoneFlexy, styles.absolute)} />
+        <img src="/violet-shape.png" alt="violet-shape" className={clsx(styles.shape, styles.absolute, styles.desktop)} />
+        <img src="/violet-shape-mobile.png" alt="violet-shape"  className={clsx(styles.shape, styles.absolute, styles.mobile)} />
+        <img src="/arrow-pattern.png" alt="arrow-pattern"  className={clsx(styles.arrows, styles.absolute)}  />
+        <img src="/house.png" alt="white-house"  className={clsx(styles.house, styles.absolute)}  />
+        <img src="/violet-smile.png" alt="violet-smile"  className={clsx(styles.violetSmile,styles.smiles,styles.smilesLeftTop, styles.absolute)} />
+        <img src="/blue-smile.png" alt="blue-smile"  className={clsx(styles.blueSmile,styles.smiles, styles.smilesLeftTop, styles.absolute)}  />
+        <img src="/green-smile.png" alt="green-smile"  className={clsx(styles.greenSmile,styles.smiles,styles.smilesLeftTop, styles.absolute)}  />
+        <img src="/white-smile.png" alt="white-smile"  className={clsx(styles.whiteSmile,styles.smiles,styles.smilesBottom, styles.absolute)}  />
+        <img src="/green-smile.png" alt="green-smile"  className={clsx(styles.greenSmile2,styles.smiles,styles.smilesRight,  styles.absolute)}  />
+        <img src="/white-smile.png" alt="white-smile"  className={clsx(styles.whiteSmile2,styles.smiles, styles.smilesRight, styles.absolute)}  />
+        <img src="/circle-text-building-landing.png" alt="circle-text"  className={clsx(styles.circleText, styles.absolute)}  />
+        <img src="/phone-flexy-home.png" alt="phone-flexy"  className={clsx(styles.phoneFlexy, styles.absolute)} />
         <div className={styles.containerStroke}> 
           <TextBackground number={8} text="arrienda" animation  className={clsx(styles.textStrokeTop, styles.desktop)}/>
           <TextBackground number={8} text="vende" animation className={clsx(styles.textStrokeBottom, styles.desktop)}/>
@@ -46,7 +34,7 @@ export default function Page(): JSX.Element {
           <TextBackground number={4} text="vende" animation className={clsx( styles.mobile)}/>
         </div>
        </div>
-       <Image src={Flexy} alt="logo" className={clsx(styles.logoSmall,styles.mobile)} />
+       <img src="/flexy.png" alt="logo"  className={clsx(styles.logoSmall,styles.mobile)} />
   
       </section>
     </main>
